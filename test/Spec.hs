a3198e1deb10e9a820712d2b10888a34e42f778b
Ziphil <yfe23457@nifty.com>
2019-03-09 20:41:09 +0900

    更新

--- test/Spec.hs
+++ test/Spec.hs
@@ -12,16 +12,16 @@ import Prelude
 
 singleMoveTest :: SpecWith (Arg Expectation)
 singleMoveTest = it "returns the resulted board after a single move" $ do
-  shouldBe stone ((2, 0), LeftTop)
+  stone `shouldBe` ((2, 0), LeftTop)
     where
       board = initialBoard <!~ ((1, 0), Tile 25 Inverse)
       stone = stones board !! 0
 
 multipleMoveTest :: SpecWith (Arg Expectation)
 multipleMoveTest = it "returns the resulted board after multiple moves" $ do
-  shouldBe firstStone ((1, 1), TopLeft)
-  shouldBe secondStone ((1, 2), TopRight)
-  shouldBe thirdStone ((2, 2), LeftBottom)
+  firstStone `shouldBe` ((1, 1), TopLeft)
+  secondStone `shouldBe` ((1, 2), TopRight)
+  thirdStone `shouldBe` ((2, 2), LeftBottom)
     where
       firstBoard = initialBoard <!~ ((1, 0), Tile 31 None)
       secondBoard = firstBoard <!~ ((1, 1), Tile 30 Clock)
@@ -32,8 +32,8 @@ multipleMoveTest = it "returns the resulted board after multiple moves" $ do
 
 invalidMoveTest :: SpecWith (Arg Expectation)
 invalidMoveTest = it "fails when an invalid move is passed" $ do
-  shouldThrow (evaluate board) anyException
-    where board = initialBoard <!~ ((2, 2), Tile 17 None)
+  evaluate board `shouldThrow` anyException
+    where board = initialBoard <<~ ((2, 2), Tile 17 None)
 
 test :: SpecWith ()
 test = describe "Tsuro.Base" $ do
