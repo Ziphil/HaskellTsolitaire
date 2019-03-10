d33e4f62ab1cdc89b466df0267104e2b9f661998
Ziphil <yfe23457@nifty.com>
2019-03-10 14:37:21 +0900

    リファクタリング

--- source/Tsuro/Read.hs
+++ source/Tsuro/Read.hs
@@ -5,8 +5,8 @@ module Tsuro.Read where
 
 import Control.Monad
 import Data.Ix
+import Data.Char
 import Data.List
-import Text.Regex
 import Tsuro.Base
 import ZiphilUtil
 
@@ -21,23 +21,11 @@ instance ReadRec Rotation where
   readRec "L" = Just Anticlock
   readRec _ = Nothing
 
-(=~) :: String -> String -> Maybe [String]
-string =~ pattern = 
-  case matchRegexAll (mkRegex pattern) string of
-    Nothing -> Nothing
-    Just (_, _, _, list) -> Just list
-
 instance ReadRec TilePos where
-  readRec string =
-    case string =~ "^([0-9])([A-Za-z])$" of
-      Nothing -> Nothing
-      Just list -> liftMaybe (parseX $ list !! 1, parseY $ list !! 0)
-        where
-          parseX string = elemIndex (head string) "ABCDEF"
-          parseY string = guard (inRange (1, 6) $ read string) >> Just (read string - 1)
+  readRec string = guard (length string == 2) >> liftMaybe (parseX $ string !! 1, parseY $ string !! 0)
+    where
+      parseX char = elemIndex char "ABCDEF"
+      parseY char = guard (inRange (1, 6) $ digitToInt char) >> Just (digitToInt char - 1)
 
 instance ReadRec GameMove where
-  readRec string = 
-    case string =~ "^([0-9][A-Za-z])([A-Za-z])$" of
-      Nothing -> Nothing
-      Just list -> liftMaybe (readRec $ list !! 0, readRec $ list !! 1)
+  readRec string = guard (length string == 3) >> liftMaybe (readRec [string !! 0, string !! 1], readRec [string !! 2])
