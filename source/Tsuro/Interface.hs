9ae102e7d6bfad8688bb016c18038b81447990e9
Ziphil <yfe23457@nifty.com>
2019-03-10 13:58:39 +0900

    インターフェースの出力を変更

--- source/Tsuro/Interface.hs
+++ source/Tsuro/Interface.hs
@@ -18,19 +18,20 @@ start = do
 showInputString :: Game -> String
 showInputString game = either (const "") show' (number <$> nextHand game)
   where
-    show' = interpose "<?> " " -> " . style Underline . show
+    show' = interpose "<?> " " -> " . show
 
 loop :: Game -> IO ()
 loop game = do
   putStrLn $ showRec game
   case (isCleared game, isOver game) of
-    (True, _) -> putStrLn $ color Magenta $ "# Congratulations! You win the game."
+    (True, _) -> putStrLn $ color Magenta $ "@ Congratulations! You win the game."
     (False, True) -> do
       putStr $ color Yellow $ showInputString game
       putStrLn "---"
-      putStrLn $ color Magenta $ "# Game over! Try again!"
+      putStrLn $ color Magenta $ "@ Game over! Try again!"
     (False, False) -> do
       nextGame <- getNextGame game
+      putStrLn ""
       loop nextGame
 
 inputGameMove :: Game -> IO GameMove
@@ -39,7 +40,7 @@ inputGameMove game = do
   input <- getLine
   case readRec input of
     Nothing -> do
-      putStrLn $ color Red $ "! Invalid input. Specify the position and rotation in the form like '5FR' or '1BT'."
+      putStrLn $ color Red $ "@ Invalid input. Specify the position and rotation in the form like '5FR' or '1BT'."
       inputGameMove game
     Just move -> return move
 
@@ -48,12 +49,12 @@ getNextGame game = do
   move <- inputGameMove game
   case applyMove move game of
     Left OutOfBoard -> do
-      putStrLn $ color Red $ "! Some stone will go out of the board."
+      putStrLn $ color Red $ "@ Some stone will go out of the board."
       getNextGame game
     Left TileAlreadyPut -> do
-      putStrLn $ color Red $ "! Some tile is already put there."
+      putStrLn $ color Red $ "@ Some tile is already put there."
       getNextGame game
     Left DetachedTilePos -> do
-      putStrLn $ color Red $ "! The specified position is not adjacent to any stone."
+      putStrLn $ color Red $ "@ The specified position is not adjacent to any stone."
       getNextGame game
     Right game -> return game
