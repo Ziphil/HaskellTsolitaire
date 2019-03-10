d2b898ab4960a648562e1c19b0976c7a4fcc9c2e
Ziphil <yfe23457@nifty.com>
2019-03-10 13:49:14 +0900

    インターフェースでの出力に色を付けるよう変更

--- source/Tsuro/Interface.hs
+++ source/Tsuro/Interface.hs
@@ -7,6 +7,7 @@ import Tsuro.Base
 import Tsuro.Read
 import Tsuro.Show
 import System.Random
+import System.Console.Pretty
 
 
 start :: IO ()
@@ -14,31 +15,45 @@ start = do
   gen <- getStdGen
   loop $ initialGame gen
 
+showInputString :: Game -> String
+showInputString game = either (const "") show' (number <$> nextHand game)
+  where
+    show' = interpose "<?> " " -> " . style Underline . show
+
 loop :: Game -> IO ()
 loop game = do
   putStrLn $ showRec game
   case (isCleared game, isOver game) of
-    (True, _) -> putStrLn "Congratulations! You win the game."
+    (True, _) -> putStrLn $ color Magenta $ "# Congratulations! You win the game."
     (False, True) -> do
-      putStrLn $ either (const "") (interpose "<?> " " -> ---" . showRec) (nextHand game)
-      putStrLn "Game over! Try again!"
+      putStr $ color Yellow $ showInputString game
+      putStrLn "---"
+      putStrLn $ color Magenta $ "# Game over! Try again!"
     (False, False) -> do
       nextGame <- getNextGame game
       loop nextGame
 
 inputGameMove :: Game -> IO GameMove
 inputGameMove game = do
-  putStr $ either (const "") (interpose "<?> " " -> " . showRec) (nextHand game)
+  putStr $ color Yellow $ showInputString game
   input <- getLine
   case readRec input of
-    Nothing -> putStrLn "! Invalid input." >> inputGameMove game
+    Nothing -> do
+      putStrLn $ color Red $ "! Invalid input. Specify the position and rotation in the form like '5FR' or '1BT'."
+      inputGameMove game
     Just move -> return move
 
 getNextGame :: Game -> IO Game
 getNextGame game = do
   move <- inputGameMove game
   case applyMove move game of
-    Left OutOfBoard -> putStrLn "! Some stone will go out of the board." >> getNextGame game
-    Left TileAlreadyPut -> putStrLn "! Some tile is already put there." >> getNextGame game
-    Left DetachedTilePos -> putStrLn "! The specified position is not adjacent to any stone." >> getNextGame game
+    Left OutOfBoard -> do
+      putStrLn $ color Red $ "! Some stone will go out of the board."
+      getNextGame game
+    Left TileAlreadyPut -> do
+      putStrLn $ color Red $ "! Some tile is already put there."
+      getNextGame game
+    Left DetachedTilePos -> do
+      putStrLn $ color Red $ "! The specified position is not adjacent to any stone."
+      getNextGame game
     Right game -> return game
