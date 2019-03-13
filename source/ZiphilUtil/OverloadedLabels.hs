7a001b76f1856acc48dc2e718611bafa37e826c2
Ziphil <yfe23457@nifty.com>
2019-03-13 17:40:09 +0900

    同名のフィールドをもつレコードを扱うユーティリティを作成

---
+++ source/ZiphilUtil/OverloadedLabels.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+
+module ZiphilUtil.OverloadedLabels where
+
+import Data.Proxy
+import GHC.OverloadedLabels
+import GHC.TypeLits
+
+
+class Has (l :: Symbol) a b | a l -> b where
+  from :: Proxy l -> a -> b
+
+instance Has l a b => IsLabel l (a -> b) where
+  fromLabel = from (Proxy :: Proxy l)
