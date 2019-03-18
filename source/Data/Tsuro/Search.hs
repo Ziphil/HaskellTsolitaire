afcee6fa6c6c725f7db321558797fdef1c94e027
Ziphil <yfe23457@nifty.com>
2019-03-18 23:13:50 +0900

    次の良さそうな手を返す関数を実装

--- source/Data/Tsuro/Search.hs
+++ source/Data/Tsuro/Search.hs
@@ -26,13 +26,17 @@ infixl 9 //^
     then next : xs
     else x : xs //^ (i - 1, next)
 
-type Label = Either GameState Board
+type BoardInfo = (Board, GameMove)
+type Label = Either GameState BoardInfo
 
 data SearchTree = Node {label :: Label, num :: Int, accum :: Double, children :: [SearchTree]}
 
 -- 葉ノードから子ノードを展開せずにプレイアウトする回数の閾値を返します。
 thresholdNum :: Int
 thresholdNum = 500
+
+searchNum :: Int
+searchNum = 100000
 
 expParam :: Double
 expParam = 1.41
@@ -55,6 +59,15 @@ correction parent child =
 score :: SearchTree -> SearchTree -> Double
 score parent child = ratio child + correction parent child
 
+search :: MonadRandom m => GameState -> m GameMove
+search state = snd . fromRight undefined . label . maximumBy (comparing num) . children <$> result
+  where
+    result = iterationList !! searchNum
+    iterationList = iterate ((fst <$>) . (montecarlo =<<)) (return $ initialSearchTree state)
+
+initialSearchTree :: GameState -> SearchTree
+initialSearchTree state = Node (Left state) 0 0 []
+
 -- 指定されたノードからモンテカルロ木探索を 1 ステップ実行し、実行後のノードとプレイアウトの報酬値を返します。
 montecarlo :: MonadRandom m => SearchTree -> m (SearchTree, Double)
 montecarlo node@(Node label num accum children) =
@@ -75,16 +88,16 @@ montecarloExpand (Node label num accum _) = montecarloRecursion nextTree
     nextTree = Node label num accum (makeChildren label)
 
 makeChildren :: Label -> [SearchTree]
-makeChildren = either makeChildrenGS makeChildrenB
+makeChildren = either makeChildrenGS makeChildrenBI
 
 makeChildrenGS :: GameState -> [SearchTree]
-makeChildrenGS state@(GameState board hand) = map (makeNode . makeBoard) $ possibleMoves' state
+makeChildrenGS state@(GameState board hand) = map makeNode $ possibleMoves' state
   where
-    makeNode board = Node (Right board) 0 0 []
+    makeNode move = Node (Right (makeBoard move, move)) 0 0 []
     makeBoard move = fromRight undefined $ applyMove' move state
 
-makeChildrenB :: Board -> [SearchTree]
-makeChildrenB board = map makeNode $ remainingTiles board
+makeChildrenBI :: BoardInfo -> [SearchTree]
+makeChildrenBI (board, _) = map makeNode $ remainingTiles board
   where
     makeNode tile = Node (Left (GameState board tile)) 0 0 []
 
@@ -97,7 +110,7 @@ montecarloRecursion node@(Node label num accum children) = (makeTree &&& snd) <$
 -- 指定された状態からプレイアウトを実行し、その結果となる報酬値を返します。
 -- 報酬値は 0 以上 1 以下の数で、1 に近いほどプレイヤーにとって有利であったことを示します。
 playout :: MonadRandom m => Label -> m Double
-playout = either playoutGS playoutB
+playout = either playoutGS (playoutB . fst)
 
 pick :: MonadRandom m => [a] -> m a
 pick list = (list !!) <$> getRandomR (0, length list - 1)
@@ -123,6 +136,3 @@ playoutB :: MonadRandom m => Board -> m Double
 playoutB board = ((/ max) . fromIntegral) <$> playoutB' board
   where
     max = fromIntegral $ length $ remainingTiles board
-
-search :: MonadRandom m => GameState -> m GameMove
-search state = undefined
