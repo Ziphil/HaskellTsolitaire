884dfbd0cb235be8961a3e93d853be5fa5bfb257
Ziphil <yfe23457@nifty.com>
2019-03-21 14:36:03 +0900

    テスト用の演算子を追加

--- test/Spec.hs
+++ test/Spec.hs
@@ -8,6 +8,7 @@ import Control.Monad
 import Data.List
 import Data.Tsuro
 import Test.Hspec
+import Ziphil.Util.Test
 
 
 infixl 6 <@
@@ -28,49 +29,49 @@ infixl 6 <<@@
 
 singleMoveTest :: SpecWith (Arg Expectation)
 singleMoveTest = it "returns the resulted board after a single move" $ do
-  stone `shouldBe` Right ((2, 0), LeftTop)
+  stone ?== Right ((2, 0), LeftTop)
     where
       board = initialBoard <@ ((1, 0), Tile 25 Inverse)
       stone = (!! 0) . stones <$> board
 
 multipleMoveTest :: SpecWith (Arg Expectation)
 multipleMoveTest = it "returns the resulted board after multiple moves" $ do
-  stone `shouldBe` Right ((2, 2), LeftBottom)
+  stone ?== Right ((2, 2), LeftBottom)
     where
       board = initialBoard <@ ((1, 0), Tile 31 None) <<@ ((1, 1), Tile 30 None) <<@ ((1, 2), Tile 21 Clock)
       stone = (!! 0) . stones <$> board
 
 detachedTilePosMoveTest :: SpecWith (Arg Expectation)
 detachedTilePosMoveTest = it "fails when attempting to put a tile at a position adjacent to no stones" $ do
-  board `shouldBe` Left DetachedTilePos
+  board ?== Left DetachedTilePos
     where
       board = initialBoard <@ ((2, 2), Tile 17 None)
 
 outOfBoardMoveTest :: SpecWith (Arg Expectation)
 outOfBoardMoveTest = it "fails when attempting to put a tile which leads a stone out of the board" $ do
-  board `shouldBe` Left OutOfBoard
+  board ?== Left OutOfBoard
     where
       board = initialBoard <@ ((5, 1), Tile 11 None) <<@ ((5, 2), Tile 8 Inverse)
 
 tileAlreadyPutMoveTest :: SpecWith (Arg Expectation)
 tileAlreadyPutMoveTest = it "fails when attempting to put a tile where some tile is already put" $ do
-  board `shouldBe` Left TileAlreadyPut
+  board ?== Left TileAlreadyPut
     where
       board = initialBoard <@ ((1, 0), Tile 25 None) <<@ ((1, 0), Tile 4 Anticlock)
 
 overTest :: SpecWith (Arg Expectation)
 overTest = it "reports that the game is over when the next tile cannot be put" $ do
-  isOver <$> game `shouldBe` Right True
+  isOver <$> game ?== Right True
     where
       firstGame = createGame $ take tileSize $ nub $ [33, 32, 0] ++ [0 ..]
       game = firstGame <@@ ((4, 0), None) <<@@ ((0, 1), Clock)
 
 wholePlayTest :: SpecWith (Arg Expectation)
 wholePlayTest = it "" $ do
-  hand `shouldBe` Right (Tile 29 None)
-  possibleMoves <$> game `shouldBe` Right [((2, 1), Clock)]
-  isOver <$> game `shouldBe` Right False
-  isCleared <$> finalGame `shouldBe` Right True
+  hand ?== Right (Tile 29 None)
+  possibleMoves <$> game ?== Right [((2, 1), Clock)]
+  isOver <$> game ?== Right False
+  isCleared <$> finalGame ?== Right True
     where
       firstGame = createGame [31, 25, 34, 30, 24, 7, 28, 33, 26, 2, 18, 5, 14, 32, 17, 21, 16, 0, 19, 6, 8, 10, 13, 23, 27, 3, 20, 9, 11, 15, 1, 12, 4, 22, 29]
       game = firstGame
