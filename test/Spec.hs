4ad4d18a3f99ceed781df920e12df6acaaddc162
Ziphil <yfe23457@nifty.com>
2019-03-21 10:17:18 +0900

    テストを追加

--- test/Spec.hs
+++ test/Spec.hs
@@ -60,11 +60,24 @@ tileAlreadyPutMoveTest = it "fails when attempting to put a tile where some tile
 
 overTest :: SpecWith (Arg Expectation)
 overTest = it "reports that the game is over when the next tile cannot be put" $ do
-  isGameOver `shouldBe` Right True
+  isOver <$> game `shouldBe` Right True
     where
       firstGame = createGame $ take tileSize $ nub $ [33, 32, 0] ++ [0 ..]
       game = firstGame <@@ ((4, 0), None) <<@@ ((0, 1), Clock)
-      isGameOver = isOver <$> game
+
+wholePlayTest :: SpecWith (Arg Expectation)
+wholePlayTest = it "" $ do
+  possibleMoves <$> game `shouldBe` Right [((2, 1), Clock)]
+  isCleared <$> finalGame `shouldBe` Right True
+    where
+      firstGame = createGame [31, 25, 34, 30, 24, 7, 28, 33, 26, 2, 18, 5, 14, 32, 17, 21, 16, 0, 19, 6, 8, 10, 13, 23, 27, 3, 20, 9, 11, 15, 1, 12, 4, 22, 29]
+      game = firstGame
+        <@@ ((4, 0), None) <<@@ ((1, 5), Inverse) <<@@ ((0, 4), None) <<@@ ((1, 0), Clock) <<@@ ((4, 5), Clock) <<@@ ((0, 3), Anticlock) <<@@ ((5, 1), Inverse)
+        <<@@ ((0, 1), None) <<@@ ((1, 3), Clock) <<@@ ((5, 0), None) <<@@ ((4, 1), None) <<@@ ((0, 5), Clock) <<@@ ((5, 5), Anticlock) <<@@ ((5, 4), None)
+        <<@@ ((4, 2), Inverse) <<@@ ((1, 1), None) <<@@ ((4, 4), Clock) <<@@ ((0, 0), None) <<@@ ((5, 3), Clock) <<@@ ((2, 5), Inverse) <<@@ ((5, 2), Anticlock)
+        <<@@ ((3, 5), Inverse) <<@@ ((1, 2), Clock) <<@@ ((0, 2), Clock) <<@@ ((2, 3), None) <<@@ ((3, 0), Anticlock) <<@@ ((3, 4), Inverse) <<@@ ((2, 0), None)
+        <<@@ ((2, 4), Clock) <<@@ ((4, 3), Inverse) <<@@ ((3, 3), None) <<@@ ((1, 4), None) <<@@ ((3, 2), None) <<@@ ((3, 1), Clock)
+      finalGame = game <<@@ ((2, 1), Clock)
 
 test :: SpecWith ()
 test = describe "Tsuro.Base" $ do
@@ -74,6 +87,7 @@ test = describe "Tsuro.Base" $ do
   outOfBoardMoveTest
   tileAlreadyPutMoveTest
   overTest
+  wholePlayTest
 
 main :: IO ()
 main = hspec test
