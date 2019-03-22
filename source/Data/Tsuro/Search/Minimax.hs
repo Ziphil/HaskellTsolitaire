d6453eb50bdf2620266d71302bd60652b40dc0cb
Ziphil <yfe23457@nifty.com>
2019-03-22 15:25:48 +0900

    ミニマックス法の基本的な枠組みを作成

--- source/Data/Tsuro/Search/Minimax.hs
+++ source/Data/Tsuro/Search/Minimax.hs
@@ -1,10 +1,58 @@
---
+{-# LANGUAGE FlexibleContexts #-}
 
 
 module Data.Tsuro.Search.Minimax where
 
+import Control.Arrow
+import Data.Either
+import Data.List
+import Data.Ord
+import Data.Reflection
 import Data.Tsuro
 
 
-search :: GameState -> GameMove
-search state = error "to be implemented"
+data Config = Config {maxDepth :: Int}
+
+type BoardLabel = (Board, GameMove)
+type Label = Either GameState BoardLabel
+
+data GameTree = Node {label :: Label, children :: [GameTree]}
+  deriving (Eq, Show)
+
+wholeGameTree :: GameState -> GameTree
+wholeGameTree = error "to be implemented"
+
+search :: Given Config => GameState -> GameMove
+search state = make $ wholeGameTree state
+  where
+    make = snd . fromRight undefined . label . maximumBy (comparing $ minimax 0) . children
+
+searchWithRatio :: Given Config => GameState -> (GameMove, Double)
+searchWithRatio state = make $ wholeGameTree state
+  where
+    make = (snd . fromRight undefined . label . fst &&& snd) . maximumBy (comparing snd) . map (id &&& minimax 0) . children
+
+minimax :: Given Config => Int -> GameTree -> Double
+minimax depth node@(Node label children) = 
+  if null children
+    then minimaxLeaf depth node
+    else if depth >= maxDepth given
+      then evaluate label
+      else minimaxRecursion depth node
+
+minimaxLeaf :: Int -> GameTree -> Double
+minimaxLeaf _ (Node label _) = either (const 0) (const 1) label
+
+minimaxRecursion :: Given Config => Int -> GameTree -> Double
+minimaxRecursion depth (Node label children) = calc $ map (minimax (depth + 1)) children
+  where
+    calc = either (const maximum) (const minimum) label
+
+evaluate :: Label -> Double
+evaluate label = either evaluateS (evaluateB . fst) label
+
+evaluateS :: GameState -> Double
+evaluateS state = error "to be implemented"
+
+evaluateB :: Board -> Double
+evaluateB board = error "to be implemented"
