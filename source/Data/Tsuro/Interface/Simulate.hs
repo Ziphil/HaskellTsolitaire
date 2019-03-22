b5e2b21a832e1006a0e8256397c5c8b56a116d1a
Ziphil <yfe23457@nifty.com>
2019-03-22 10:15:43 +0900

    AIにシミュレーションさせるためのインターフェースを作成

---
+++ source/Data/Tsuro/Interface/Simulate.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+
+module Data.Tsuro.Interface.Simulate where
+
+import Data.Tsuro
+import Data.Tsuro.Interface.Core
+import Data.Tsuro.Search.Core
+import qualified Data.Tsuro.Search.Montecarlo as Montecarlo
+import Data.Tsuro.Show
+
+
+data SomeSearch = forall s. Search IO s => SomeSearch s
+
+start :: IO ()
+start = do
+  SomeSearch search <- inputSearch
+  ((game, record), status) <- simulate search
+  flushStrLn ""
+  flushStrLn $ showRich game
+  case status of
+    Success -> do
+      flushStrLn $ colorMessage "@ Succeeded."
+      flushStrLn $ colorMessage $ "@ Record: " ++ showRich record
+    Failure -> do
+      flushStrLn $ colorMessage "@ Failed."
+      flushStrLn $ colorMessage $ "@ Record: " ++ showRich record ++ " " ++ either (const "") (show . number) (nextHand game) ++ "*"
+
+inputSearch :: IO SomeSearch
+inputSearch = do
+  flushStr $ colorInput "<?> Algorithm -> "
+  input <- getLine
+  case input of
+    "m" -> return $ SomeSearch Montecarlo.defaultConfig
+    "mf" -> return $ SomeSearch (Montecarlo.Config 1000 4 3)
+    _ -> do
+      flushStrLn $ colorError "@ No such algorithm."
+      inputSearch
