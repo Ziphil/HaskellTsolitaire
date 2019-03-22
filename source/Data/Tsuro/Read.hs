b95f18d3e9aba109d9cbabd390d80823750f74a8
Ziphil <yfe23457@nifty.com>
2019-03-22 17:27:04 +0900

    リファクタリング

--- source/Data/Tsuro/Read.hs
+++ source/Data/Tsuro/Read.hs
@@ -26,11 +26,16 @@ instance ReadRich Rotation where
   readRich "L" = Just Anticlock
   readRich _ = Nothing
 
+parseX :: Char -> Maybe Int
+parseX char = elemIndex char "ABCDEF"
+
+parseY :: Char -> Maybe Int
+parseY char = guard (isDigit char) >> guard (inRange (0, 5) y) >> Just y
+  where
+    y = digitToInt char - 1
+
 instance ReadRich TilePos where
   readRich string = guard (length string == 2) >> outA (parseX $ string !! 1, parseY $ string !! 0)
-    where
-      parseX char = elemIndex char "ABCDEF"
-      parseY char = guard (isDigit char) >> guard (inRange (1, 6) $ digitToInt char) >> Just (digitToInt char - 1)
 
 instance ReadRich GameMove where
   readRich string = guard (length string == 3) >> outA (readRich $ string !!& [0, 1], readRich $ string !!& [2])
