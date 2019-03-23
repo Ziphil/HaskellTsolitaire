d240af9e99ce9ac964244d66e5b32b5f503b576e
Ziphil <yfe23457@nifty.com>
2019-03-23 15:57:29 +0900

    インターフェースで次のタイルを変える機能を追加

--- source/Data/Tsuro/Interface/Game.hs
+++ source/Data/Tsuro/Interface/Game.hs
@@ -7,7 +7,10 @@ module Data.Tsuro.Interface.Game
   )
 where
 
+import Control.Monad
+import Data.Char
 import Data.Either
+import Data.Ix
 import Data.List
 import Data.Tsuro
 import Data.Tsuro.Interface.Util
@@ -19,6 +22,7 @@ import System.Console.Pretty
 import System.IO
 import System.Random
 import Text.Printf
+import Text.Read
 import Ziphil.Util.List
 
 
@@ -46,7 +50,7 @@ loop game = do
       flushStrLn ""
       loop nextGame
 
-inputGameMove :: Game -> IO GameMove
+inputGameMove :: Game -> IO (Either Game GameMove)
 inputGameMove game = do
   flushStr $ showInputString game
   input <- getLine
@@ -62,11 +66,20 @@ inputGameMove game = do
         (move, ratio) <- runSearchWithRatio search $ fromRight undefined (gameStateOf game)
         flushStrLn $ colorMessage $ "@ Suggested move: " ++ showRich move ++ " (" ++ printf "%.2f" (ratio * 100) ++ "%)"
         inputGameMove game
+    ':' : 'c' : ' ' : rest -> case parseTile rest of
+      Nothing -> do
+        flushStrLn $ colorError "@ Invalid input"
+        inputGameMove game
+      Just tile -> case changeNextHand tile game of
+        Left TileNotRemaining -> do
+          flushStrLn $ colorError "@ This tile is already put in the board."
+          inputGameMove game
+        Right nextGame -> return $ Left nextGame
     _ -> case readRich input of
       Nothing -> do
         flushStrLn $ colorError "@ Invalid input. Specify the position and rotation in the form like '5FR' or '1BT'."
         inputGameMove game
-      Just move -> return move
+      Just move -> return $ Right move
 
 data SomeSearch = forall s. Search IO s => SomeSearch s
 
@@ -77,17 +90,24 @@ searchOf string =
     "mf" -> Just $ SomeSearch (Montecarlo.Config 1000 4 3)
     _ -> Nothing
 
+parseTile :: String -> Maybe Tile
+parseTile string = make =<< readMaybe string
+  where
+    make number = guard (inRange (0, tileSize - 1) number) >> Just (Tile number None)
+
 getNextGame :: Game -> IO Game
 getNextGame game = do
-  move <- inputGameMove game
-  case applyMove move game of
-    Left OutOfBoard -> do
-      flushStrLn $ colorError "@ Some stone will go out of the board."
-      getNextGame game
-    Left TileAlreadyPut -> do
-      flushStrLn $ colorError "@ Some tile is already put there."
-      getNextGame game
-    Left DetachedTilePos -> do
-      flushStrLn $ colorError "@ The specified position is not adjacent to any stone."
-      getNextGame game
-    Right nextGame -> return nextGame
+  result <- inputGameMove game
+  case result of
+    Left nextGame -> return nextGame
+    Right move -> case applyMove move game of
+      Left OutOfBoard -> do
+        flushStrLn $ colorError "@ Some stone will go out of the board."
+        getNextGame game
+      Left TileAlreadyPut -> do
+        flushStrLn $ colorError "@ Some tile is already put there."
+        getNextGame game
+      Left DetachedTilePos -> do
+        flushStrLn $ colorError "@ The specified position is not adjacent to any stone."
+        getNextGame game
+      Right nextGame -> return nextGame
