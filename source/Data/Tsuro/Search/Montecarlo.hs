e547e5b84dddd6ea5cf419553d753254fb87c0f0
Ziphil <yfe23457@nifty.com>
2019-03-26 09:22:46 +0900

    モンテカルロ法で追加の報酬値を扱えるよう変更

--- source/Data/Tsuro/Search/Montecarlo.hs
+++ source/Data/Tsuro/Search/Montecarlo.hs
@@ -39,12 +39,12 @@ fastConfig = Config 1000 4 3
 type BoardLabel = (Board, GameMove)
 type Label = Either GameState BoardLabel
 
-data SearchTree = Node {label :: Label, num :: Int, accum :: Double, children :: [SearchTree]}
+data SearchTree = Node {label :: Label, num :: Int, accum :: Double, extra :: Double, children :: [SearchTree]}
   deriving (Eq, Show)
 
 -- そのノードからの探索における報酬値の平均を返します。
 ratio :: SearchTree -> Double
-ratio (Node _ num accum _) = 
+ratio (Node _ num accum _ _) = 
   if num == 0
     then 0
     else accum / fromIntegral num
@@ -61,10 +61,10 @@ correction parent child =
     childNum = fromIntegral $ num child
 
 score :: Given Config => SearchTree -> SearchTree -> Double
-score parent child = ratio child + correction parent child
+score parent child = ratio child + extra child + correction parent child
 
 initialSearchTree :: GameState -> SearchTree
-initialSearchTree state = Node (Left state) 0 0 (makeChildrenS state)
+initialSearchTree state = Node (Left state) 0 0 0 (makeChildrenS state)
 
 -- モンテカルロ木探索を規定回数だけ実行して、与えられたゲーム状況において最適と思われる手を返します。
 search :: (Given Config, MonadRandom m) => GameState -> m GameMove
@@ -89,7 +89,7 @@ montecarlo' = (fst <$>) . montecarlo
 
 -- 指定されたノードからモンテカルロ木探索を 1 ステップ実行し、実行後のノードとプレイアウトの報酬値を返します。
 montecarlo :: (Given Config, MonadRandom m) => SearchTree -> m (SearchTree, Double)
-montecarlo node@(Node label num accum children) =
+montecarlo node@(Node _ num _ _ children) =
   if null children
     then if num < thresholdNum given
       then montecarloPlayout node
@@ -97,29 +97,29 @@ montecarlo node@(Node label num accum children) =
     else montecarloRecursion node
 
 montecarloPlayout :: (Given Config, MonadRandom m) => SearchTree -> m (SearchTree, Double)
-montecarloPlayout (Node label num accum _) = (makeNode &&& id) <$> playout label
+montecarloPlayout (Node label num accum extra _) = (makeNode &&& id) <$> playout label
   where
-    makeNode reward = Node label (num + 1) (accum + reward) []
+    makeNode reward = Node label (num + 1) (accum + reward) extra []
 
 montecarloExpand :: (Given Config, MonadRandom m) => SearchTree -> m (SearchTree, Double) 
-montecarloExpand node@(Node label num accum _) = 
+montecarloExpand node@(Node label num accum extra _) = 
   if null children
     then montecarloLeaf node
     else montecarloRecursion nextNode
   where
-    nextNode = Node label num accum children
+    nextNode = Node label num accum extra children
     children = makeChildren label
 
 montecarloRecursion :: (Given Config, MonadRandom m) => SearchTree -> m (SearchTree, Double)
-montecarloRecursion node@(Node label num accum children) = (makeNode &&& snd) <$> montecarlo child
+montecarloRecursion node@(Node label num accum extra children) = (makeNode &&& snd) <$> montecarlo child
   where
-    makeNode (nextChild, reward) = Node label (num + 1) (accum + reward) (update index nextChild children)
+    makeNode (nextChild, reward) = Node label (num + 1) (accum + reward) extra (update index nextChild children)
     (index, child) = maximumBy' (comparing $ score node) children
 
 montecarloLeaf :: (Given Config, MonadRandom m) => SearchTree -> m (SearchTree, Double)
-montecarloLeaf node@(Node label num accum _) = return (nextNode, reward)
+montecarloLeaf node@(Node label num accum extra _) = return (nextNode, reward)
   where
-    nextNode = Node label (num + 1) (accum + reward) []
+    nextNode = Node label (num + 1) (accum + reward) extra []
     reward = either (const 0) (const 1) label
 
 makeChildren :: Label -> [SearchTree]
@@ -128,12 +128,12 @@ makeChildren = either makeChildrenS (makeChildrenB . fst)
 makeChildrenS :: GameState -> [SearchTree]
 makeChildrenS state@(GameState board hand) = map makeNode $ possibleMovesAndBoards' state
   where
-    makeNode (move, board) = Node (Right (board, move)) 0 0 []
+    makeNode (move, board) = Node (Right (board, move)) 0 0 0 []
 
 makeChildrenB :: Board -> [SearchTree]
 makeChildrenB board = map makeNode $ remainingTiles board
   where
-    makeNode tile = Node (Left (GameState board tile)) 0 0 []
+    makeNode tile = Node (Left (GameState board tile)) 0 0 0 []
 
 -- 指定された状態からプレイアウトを実行し、その結果となる報酬値を返します。
 -- 報酬値は 0 以上 1 以下の数で、1 に近いほどプレイヤーにとって有利であったことを示します。
